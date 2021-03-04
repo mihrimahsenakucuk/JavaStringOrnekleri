7ba321b21169d595a7ac81ca479a0779e6dea157
mihrimahsenakucuk <73707051+mihrimahsenakucuk@users.noreply.github.com>
2021-03-04 17:27:14 +0300

    Update Soru04.java

--- src/Soru04.java
+++ src/Soru04.java
@@ -4,10 +4,10 @@ public class Soru04 {
 
     public static void main(String[] args) {
 
-        String cumle="Java programlama dilidir ";
-        String reverse= cumle.substring(16)+cumle.substring(5,17)+cumle.substring(0,4);
-
-        System.out.println(reverse);
+     String cumle= "Java programlama dilidir";
+     StringBuffer sb= new StringBuffer(cumle);
+     System.out.println(sb.reverse());
+	  
 
 
     }
